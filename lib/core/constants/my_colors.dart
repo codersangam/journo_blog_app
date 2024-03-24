8e24e930cb11292948a3faf1dc80a2a4161c480d
Sangam Singh <hritriik@gmail.com>
2024-03-24 11:31:12 +0400

    updaed readme for season2

--- lib/core/constants/my_colors.dart
+++ lib/core/constants/my_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyColors {
   MyColors._();
 
-  static const Color primaryColor = Color(0xffE85520);
+  static const Color primaryColor = Colors.green;
   static const Color secondaryColor = Colors.green;
   static const Color white = Colors.white;
 }
